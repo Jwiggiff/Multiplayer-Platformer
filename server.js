5272f470094911c75fe94578ef37e025a2a3397d
Jwiggiff <friedman.josh03@gmail.com>
2018-08-23 22:10:27 -0400

    Change to port 80

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var socket = require('socket.io');
 console.log('Server has started.');
 
 var app = express();
-var server = app.listen(3000);
+var server = app.listen(80);
 app.use(express.static('public'));
 
 var io = socket(server);
